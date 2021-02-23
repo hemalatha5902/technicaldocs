3705681838145d2a5b4d9148c841fc5a24e457eb
hemalatha5902 <79440554+hemalatha5902@users.noreply.github.com>
2021-02-23 16:45:15 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'git & github documentation'
+copyright = '2021, hema latha'
+author = 'hema latha'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
